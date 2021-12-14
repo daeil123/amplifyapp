4e96d01065781d24f11411e3092fbc53465b785b
daeil123 <daeil123@hotmail.com>
2021-12-15 09:09:31 +1000

    added auth v2.9

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
 export default withAuthenticator(App);
 */
 
+/*
 import { Amplify } from 'aws-amplify';
 
 import { Authenticator } from '@aws-amplify/ui-react';
@@ -38,3 +39,38 @@ export default function App() {
     </Authenticator>
   );
 }
+*/
+
+
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ isPassedToWithAuthenticator, signOut, user }) {
+  if (!isPassedToWithAuthenticator) {
+    throw new Error(`isPassedToWithAuthenticator was not provided`);
+  }
+
+  return (
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
+}
+
+
