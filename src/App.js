1f7a84663495f40560b195d6a570a4f63950a652
daeil123 <daeil123@hotmail.com>
2021-12-15 08:54:33 +1000

    added auth v2.8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-
+//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+/*
 function App() {
   return (
     <div className="App">
@@ -16,3 +16,25 @@ function App() {
 }
 
 export default withAuthenticator(App);
+*/
+
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
